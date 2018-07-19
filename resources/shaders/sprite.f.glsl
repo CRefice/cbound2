c9b38b1c321d534d4c60b356d11213cf915b8c30
Carlo <CRefice@gmail.com>
2018-07-19 18:28:41 +0200

    Move resources out of source dir

---
+++ resources/shaders/sprite.f.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+
+// FRAGMENT SHADER
+
+uniform sampler2DArray image;
+
+in vec2 tex_coord;
+out vec4 out_color;
+
+void main() {
+	vec4 color = texture(image, tex_coord);
+	if (color.a < 0.5) discard;
+	out_color = color;
+}
